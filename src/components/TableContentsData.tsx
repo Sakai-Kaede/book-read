c875f2b714e2848889669f76576da01faa1576a8
Sakai-Kaede <kana1024k@gmail.com>
2024-03-26 22:37:53 +0900

    Implemented the data storage location for the table of contents

---
+++ src/components/TableContentsData.tsx
@@ -0,0 +1,18 @@
+export const TableContentsData = [
+  {
+    title: "1ページへ",
+    pageNum: 1,
+  },
+  {
+    title: "2ページへ",
+    pageNum: 2,
+  },
+  {
+    title: "3ページへ",
+    pageNum: 3,
+  },
+  {
+    title: "4ページへ",
+    pageNum: 4,
+  },
+]
