9d3c4ba32c9d4b8933ebd6122cff9e9179dce0dd
Sakai-Kaede <kana1024k@gmail.com>
2024-04-02 23:52:36 +0900

    Creating a dynamic table of contents

--- src/components/TableContents.tsx
+++ src/components/TableContents.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { TableContentsData } from "./TableContentsData";
 import '../css/TableContents.css'
 
@@ -6,18 +7,34 @@ type Props = {
 };
 
 const TableContents = ({ handleValueChange }: Props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className="TableContents">
-      <ul>
-        {TableContentsData.map((value, key) => (
-          <li key={key}>
-            <button  className="row" onClick={() => {handleValueChange(value.pageNum);}}>
-              {value.title}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <nav className={isOpen ? "open" : ""}>
+        <div className="TableContents">
+          <ul>
+            {TableContentsData.map((value, key) => (
+              <li key={key} className="row">
+                <button className="button" onClick={() => {handleValueChange(value.pageNum);}}>
+                  {value.title}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+
+      <div className="toggle-btn" onClick={toggleNav}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </>
   );
 };
 
