60d1441077e77f018abecbb3243a33be04594379
Sakai-Kaede <kana1024k@gmail.com>
2024-04-04 23:26:33 +0900

    Add background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import './App.css';
-import MoveButton from './components/MoveButton';
 import Story from './components/Story';
 import TableContents from './components/TableContents';
+import BookImage from "./components/BookImage";
+import MoveForwardButton from "./components/MoveForwardButton";
+import MoveBackwardButton from "./components/MoveBackwardButton";
 
 function App() {
   const [pageNum, setPageNum] = useState<number>(1);
@@ -14,10 +16,16 @@ function App() {
 
   return(
     <>
-      <MoveButton pageNum={pageNum} handleValueChange={handleValueChange}></MoveButton>
-      <p>{pageNum}</p>
-      <Story pageNum={pageNum} />
+      <div className="item">
+      <MoveForwardButton pageNum={pageNum} handleValueChange={handleValueChange}></MoveForwardButton>
+        <Story pageNum={pageNum} />
+        <MoveBackwardButton pageNum={pageNum} handleValueChange={handleValueChange}></MoveBackwardButton>
+      </div>
+      
+      <p className="pageNum">{pageNum}ページ</p>
       <TableContents handleValueChange={handleValueChange} />
+      <BookImage></BookImage>
+      <img src="../src/assets/Waterfall.jpg" alt="Waterfall" />
     </>
   );
 }
